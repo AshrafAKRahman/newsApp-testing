d24acf025412ca9285ed186f72a43d3f7045e37b
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-02-08 21:49:30 +0000

    tests are working

--- src/Components/News/News.test.js
+++ src/Components/News/News.test.js
@@ -7,3 +7,9 @@ it('checks that "Search" is on the screen', () => {
   const h1 = screen.getByText(/Search/i);
   expect(h1).toBeInTheDocument();
 });
+
+it("checks that the input bar is on the screen", () => {
+  render(<News />);
+  const input = screen.getByPlaceholderText(/Search for news.../i);
+  expect(input).toBeInTheDocument();
+});
